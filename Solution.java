8e5fce6696f856108515cfb0f4fc10a69ea6badd
cheeseburger666 <61541547+cheeseburger666@users.noreply.github.com>
2020-07-30 21:39:10 +0800

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,14 @@
+package leetcode;
+//NO.69
+
+public class Solution {
+
+    public int mySqrt(int a) {
+        long x = a;
+        while (x * x > a) {
+            x = (x + a / x) / 2;
+        }
+        return (int) x;
+    }
+}
+//Newton's method
